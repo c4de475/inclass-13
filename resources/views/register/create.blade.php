cd2dd0d771a174baa618f14b60fec138c424317a
c4de475 <cadebutler@icloud.com>
2024-04-15 15:32:46 -0400

    Fixing main application issues

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -12,13 +12,6 @@
                     @enderror
                 </div>
                 <div class="mb-3">
-                    <label for="username" class="form-label">Username:</label>
-                    <input type="text" class="form-control" id="username" name="username" value="{{ old('username') }}" placeholder="Enter Username Here...">
-                    @error('username')
-                        <p class="text-red-500">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-3">
                     <label for="email" class="form-label">Email:</label>
                     <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}" placeholder="Enter Email Here...">
                     @error('email')
